e47a282075cdfbb8f7a37018310aa9e081747805
Chad Goh <49085462+CGJL@users.noreply.github.com>
2020-04-23 18:51:39 +0800

    uibuild

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var express = require("express");
+const express = require("express");
+
 const morgan = require("morgan");
 const cors = require("cors");
 
@@ -6,6 +7,20 @@ const app = express();
 app.use(express.static("build"));
 app.use(cors());
 app.use(express.json());
+
+const errorHandler = (error, request, response, next) => {
+  console.error(">>>", error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "Invalid id format" });
+  }
+
+  next(error);
+};
+
+require("dotenv").config();
+
+const Person = require("./models/person");
 
 morgan.token("custom", (req, res) => {
   return [
@@ -19,40 +34,15 @@ morgan.token("custom", (req, res) => {
 
 app.use(morgan(":custom :response-time ms"));
 
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1,
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-343-1231231",
-    id: 2,
-  },
-  {
-    name: "Dan Abramov",
-    number: "123-123123",
-    id: 3,
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "098-9070313",
-    id: 4,
-  },
-];
-
-function generateID() {
-  const maxId = persons.length > 0 ? Math.max(...persons.map((p) => p.id)) : 0;
-
-  return maxId + 1;
-}
-
 app.get("/", (req, res) => {
   res.json({ message: "Hello!" });
 });
+
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+  Person.find({}).then((persons) => {
+    console.log(persons);
+    res.json(persons.map((person) => person.toJSON()));
+  });
 });
 
 app.get("/info", (req, res) => {
@@ -63,52 +53,66 @@ app.get("/info", (req, res) => {
   );
 });
 
-app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).end();
-  }
+app.get("/api/persons/:id", (req, res, next) => {
+  Person.findById(req.params.id)
+    .then((person) => {
+      if (person) {
+        res.json(person.toJSON());
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
 app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter((note) => note.id !== id);
-
-  res.status(204).end();
+  Person.findByIdAndRemove(req.params.id)
+    .then((result) => {
+      res.status(204).end();
+    })
+    .catch((err) => next(err));
 });
 
 app.post("/api/persons", (req, res) => {
   const body = req.body;
 
-  let listOfNames = persons.map((person) => person.name);
-  if (
-    listOfNames
-      .map((name) => name.toLowerCase())
-      .includes(body.name.toLowerCase())
-  ) {
-    return res.status(400).json({
-      error: "name alr exists",
+  Person.exists({ name: body.name })
+    .then((result) => {
+      if (result) {
+        Person.findOneAndUpdate(
+          { name: body.name },
+          { $set: { number: body.number } },
+          (err, doc) => {
+            if (err) {
+              return res.status(400).end();
+            } else {
+              return res.status(200).json({ msg: "user updated", user: doc });
+            }
+          }
+        );
+      } else {
+        if (!body.name || !body.number) {
+          return res.status(400).json({
+            error: "name or number missing",
+          });
+        }
+
+        const person = new Person({
+          name: body.name,
+          number: body.number,
+        });
+
+        person.save().then((savedPerson) => {
+          res.json(savedPerson.toJSON());
+        });
+      }
+    })
+    .catch((err) => {
+      console.log("error occured when checking if user exists:", err);
+      return res.send(400);
     });
-  }
-
-  if (!body.name || !body.number) {
-    return res.status(400).json({
-      error: "name or number missing",
-    });
-  }
-
-  const person = {
-    name: body.name,
-    number: body.number,
-    id: generateID(),
-  };
-
-  persons = persons.concat(person);
-  res.json(person);
 });
 
 const unknownEndpoint = (req, res) => {
@@ -116,7 +120,9 @@ const unknownEndpoint = (req, res) => {
 };
 
 app.use(unknownEndpoint);
-const PORT = process.env.PORT || 3001;
+
+app.use(errorHandler);
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`server running on ${PORT}`);
 });
